1562e59bc593523ff120460b213eab3130a7c60e
Wired361 <justin.parker.12@gmail.com>
2019-09-03 22:46:34 -0500

    removed redundant Activities directory

--- Activities/02-Stu_Flags/Solved/easyish.sh
+++
@@ -1,5 +0,0 @@
-# In order to run this you will need to change the permissions to allow for execution: chmod 777 challenge.sh and execute with sh easyish.sh
-# Reference for above: https://www.maketecheasier.com/file-permissions-what-does-chmod-777-means/
-
-mkdir -p SpaceX/API/Launches SpaceX/API/Capsules
-curl https://api.spacexdata.com/v3/launches/latest?pretty=true > SpaceX/API/Launches/Amos-17.json
